e12735d776b22cbbe5fbb14fc5f6d0a292bc2053
Stefan Wapnick <stefanwapnick@hotmail.com>
2018-02-15 15:13:03 -0500

    Added book controller and routing

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BooksController extends Controller {
+
+    // HTTP GET all books
+    public Result getBooks(){
+        return TODO;
+    }
+
+    // HTTP GET specific book
+    public Result getBook(Integer id){
+        return TODO;
+    }
+
+    // HTTP GET create book view
+    public Result getCreate(){
+        return TODO;
+    }
+
+    // HTTP POST create new book
+    public Result postCreate(){
+        return TODO;
+    }
+
+    // HTTP GET retrieve edit view
+    public Result getEdit(Integer id){
+        return TODO;
+    }
+
+    // HTTP PUT update book
+    public Result PutEdit(Integer id){
+        return TODO;
+    }
+
+    // HTTP DELETE
+    public Result delete(Integer id){
+        return TODO;
+    }
+
+
+}
